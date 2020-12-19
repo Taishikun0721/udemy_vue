24d98e6e3361920061a91d88de50f86d9a982871
Taishiro Inokuchi <tai0721abc@gmail.com>
2020-12-19 20:54:01 +0900

    シングルファイルコンポーネントを作った

---
+++ src/components/LikeNumber.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+  <p>いいね({{number}})</p>
+  <button @click="increment">buttonz1</button>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      number: 20
+	};
+  },
+  methods: {
+		increment() {
+			this.number += 1
+		}
+	}
+};
+</script>
+
+<style scoped>
+  div {
+    border: 1px solid red;
+  }
+</style>
